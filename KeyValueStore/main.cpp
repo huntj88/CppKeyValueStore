c09806a71e39ef127e47d451b08be92a6a8b207c
James Hunt <jhunt@inmotionsoftware.com>
2019-04-14 11:33:20 -0500

    Initial Commit

---
+++ KeyValueStore/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KeyValueStore
+//
+//  Created by James Hunt on 4/14/19.
+//  Copyright © 2019 James Hunt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
